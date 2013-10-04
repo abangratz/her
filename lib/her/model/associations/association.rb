a91c182aebbcb83c27da07c60d5b69b60ce355ca
Anton Bangratz <anton.bangratz@gmail.com>
2013-10-04 11:57:59 +0200

    Add erroneously deleted line back

--- lib/her/model/associations/association.rb
+++ lib/her/model/associations/association.rb
@@ -37,6 +37,7 @@ module Her
           return @opts[:default].try(:dup) if @parent.attributes.include?(@name) && @parent.attributes[@name].empty? && @params.empty?
 
           if @parent.attributes[@name].blank? || @params.any?
+            path = build_association_path lambda { "#{@parent.request_path(@params)}#{@opts[:path]}" }
             @klass.get(path, @params.merge(page: page, per_page: per_page, mode: :paginate))
           else
             @parent.attributes[@name]
